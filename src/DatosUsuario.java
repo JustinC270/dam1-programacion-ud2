ef176f5f9748a8d74d4272ff9bb3fc1bdce6164d
JustinC270 <justincaamano770@gmail.com>
2025-10-01 14:11:07 +0200

    Email válido o no válido

--- src/DatosUsuario.java
+++ src/DatosUsuario.java
@@ -1,6 +1,26 @@
-public class DatosUsuario {
+import java.util.Scanner;
 
+import utilidades.ProcesadorTexto;
+
+public class DatosUsuario {
+ 
     public static void main(String[] args) {
-        
-    }
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Tu nombre es: " );
+         String nombre = sc.nextLine();
+         System.out.println("Tu edad: " );
+         int edad = sc.nextInt();
+         System.out.println("Tu email:"  );
+         String correo = sc.nextLine();
+         String gmail = sc.nextLine();
+         boolean emailValido = ProcesadorTexto.esEmailValido(gmail);
+         if(emailValido){
+            System.out.println("Tu email es válido");
+         }else{
+            System.out.println("Tu email NO es válido");
+         }
+            sc.close();
+        }
 }
+
+
