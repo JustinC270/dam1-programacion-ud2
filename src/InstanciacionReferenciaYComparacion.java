88f43bb1853914ba8bbb07e840f8c04932837fcc
JustinC270 <justincaamano770@gmail.com>
2025-10-03 14:14:33 +0200

    cancion

--- src/InstanciacionReferenciaYComparacion.java
+++ src/InstanciacionReferenciaYComparacion.java
@@ -1,1 +1,20 @@
+import utilidades.Cancion;
+import utilidades.Pelicula;
 
+public class InstanciacionReferenciaYComparacion {
+
+    public static void main(String[] args) {
+        Cancion c1 = new Cancion();
+        Cancion c2 = new Cancion("Clocks", "Coldplay");
+        System.out.println(c2);
+        System.out.println(c1 == c1);
+        Cancion c3 = new Cancion("hola", "alonso");
+        Cancion c4 = c3;
+        System.out.println(c4 == c3);
+        System.out.println(c3);
+        System.out.println(c3.equals(c4));
+        Pelicula p1 = new Pelicula();
+        Pelicula p2 = new Pelicula();
+        
+    }
+}
