a46e3ef9a39ea01d5acbb6fbbdd8aa3fb2fd4bf7
JustinC270 <874@alumnos.fpsumma.es>
2025-10-20 19:51:26 +0200

    metodos

--- src/TrabajandoConMetodos.java
+++ src/TrabajandoConMetodos.java
@@ -10,25 +10,31 @@ public class TrabajandoConMetodos {
         // ================================
         // 1. Llama al método saludar() de la clase DemoMetodos.
         // Tu código aquí ↓
-        DemoMetodos saludar();
+            method.saludar();
 
         // ================================
         // EJERCICIO 2: Métodos con retorno
         // ================================
         // 1. Llama al método sumar(5, 3).
         // 2. Guarda el resultado en una variable e imprímelo.
+        int resultadoSuma = method.sumar(5,3);
+        System.out.println("Resultado de la suma: " + resultadoSuma);
         // 3. Llama al método presentar("Lucía") e imprime el resultado.
         // Tu código aquí ↓
+        String mensaje = method.presentar("Lucia");
+        System.out.println(mensaje);
         
-        DemoMetodos.sumar(5,3);
        
 
         // ================================
         // EJERCICIO 3: Paso por valor (primitivos)
         // ================================
         // 1. Declara una variable int n = 10.
+        int n = 10;
         // 2. Llama a method.incrementar(n).
+        method.incrementar(n);
         // 3. Imprime n después de llamar al método.
+        System.out.println("Valor de n despues de llamr a incrementar(): " + n);
         // Tu código aquí ↓
 
 
@@ -37,8 +43,11 @@ public class TrabajandoConMetodos {
         // ================================
         // 1. Crea un objeto Cancion con título "Original" - Usa el constructor parametrizado
         //  que recibe 2 argumentos: título y artista.
+        Cancion cancion = new Cancion("Original", "Carlos");
         // 2. Llama a method.cambiarTitulo(cancion).
+        method.cambiarTitulo(cancion);
         // 3. Imprime la canción después.
+        System.out.println("Despues de cambiar el título: " + cancion);
         // Tu código aquí ↓
 
     }
